eea9e5da20da58789ce9a2a95cec964b49ab3803
Yamashita Wakako <11wakakodesu13@gmail.com>
2023-04-21 07:32:10 +0000

    laravel14

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,35 +12,42 @@
             <div class="col-md-8 mx-auto">
                 <h2>MYプロフィール</h2>
                 <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data">
-                <div class="form-group row">
-                    <label class="col-md-2">氏名(name)</label>
-                    <div class="col-md-10">
-                        <textarea class="form-control" name="name" rows="1">{{ old('name') }}</textarea>
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名(name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-2">性別(gender)</label>
-                    <div class="col-md-10">
-                        <textarea class="form-control" name="gender" rows="1">{{ old('gender') }}</textarea>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別(gender)</label>
+                        <div class="col-md-10">
+                            <label><input type="radio" name="gender" value="male">男性</label>
+                            <label><input type="radio" name="gender" value="female">女性</label>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-2">趣味(hobby)</label>
-                    <div class="col-md-10">
-                        <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味(hobby)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-2">自己紹介欄(introduction)</label>
-                    <div class="col-md-10">
-                        <textarea class="form-control" name="introduction" rows="10">{{ old('in') }}</textarea>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('in') }}</textarea>
+                        </div>
                     </div>
-                </div>
+                    @csrf
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
-
-
-  
-                    
 @endsection
